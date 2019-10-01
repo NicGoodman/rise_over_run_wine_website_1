de88a34b2df11252734f725c1acefd9073c897e9
Nic <nic@ninebarcreative.com>
2019-10-01 12:04:20 -0400

    changed db variables

--- config/db.php
+++ config/db.php
@@ -10,10 +10,10 @@
 
 return [
     'driver' => getenv('DB_DRIVER'),
-    'server' => getenv('DB_SERVER'),
+    'server' => getenv('DB_HOST'),
     'user' => getenv('DB_USER'),
     'password' => getenv('DB_PASSWORD'),
-    'database' => getenv('DB_DATABASE'),
+    'database' => getenv('DB_NAME'),
     'schema' => getenv('DB_SCHEMA'),
     'tablePrefix' => getenv('DB_TABLE_PREFIX'),
     'port' => getenv('DB_PORT')
